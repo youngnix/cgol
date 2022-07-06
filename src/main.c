204bce0c39f8697249f04dc01635ca1f4430cf25
anddsz <neraidwastaken@gmail.com>
2022-07-06 11:50:57 -0300

    cpp bad

--- src/main.c
+++ src/main.c
@@ -1,15 +1,14 @@
-#include <SDL2/SDL.h>
-#include <iostream>
-#include <random>
-#include <ctime>
 #include <stdio.h>
-#include <typeinfo>
+#include <stdlib.h>
+#include <time.h>
+#include <stdbool.h>
+#include <SDL2/SDL.h>
 
 int main(int argc, char** argv){
     srand(time(NULL));
     
     if(SDL_Init(SDL_INIT_EVERYTHING) != 0){
-        std::cout << "SDL couldn't start" << std::endl;
+        printf("Shit happens. SDL error: %s\n", SDL_GetError());
     }
 
     SDL_Window* window = SDL_CreateWindow("Title", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 1280, 720, 0);
@@ -17,22 +16,22 @@ int main(int argc, char** argv){
 
     SDL_Surface* boardSurface = SDL_LoadBMP("res\\board.bmp");
 
-    const unsigned int mapWidth = (unsigned int)boardSurface->w;
-    const unsigned int mapHeight = (unsigned int)boardSurface->h;
+    const unsigned int MAP_WIDTH = boardSurface->w;
+    const unsigned int MAP_HEIGHT = boardSurface->h;
 
-    unsigned char firstBoard[mapWidth][mapHeight];
-    unsigned char secondBoard[mapWidth][mapHeight];
+    unsigned char firstBoard[MAP_WIDTH][MAP_HEIGHT];
+    unsigned char secondBoard[MAP_WIDTH][MAP_HEIGHT];
 
-    unsigned char (*firstPointer)[mapHeight] = firstBoard;
-    unsigned char (*secondPointer)[mapHeight] = secondBoard;
+    unsigned char (*firstPointer)[MAP_HEIGHT] = firstBoard;
+    unsigned char (*secondPointer)[MAP_HEIGHT] = secondBoard;
 
-    for(int i = 0; i <= mapHeight; i++){
-        for(int j = 0; j <= mapWidth; j++){
+    for(int i = 0; i <= MAP_HEIGHT; i++){
+        for(int j = 0; j <= MAP_WIDTH; j++){
             break;
         }
     }
 
-    SDL_RenderSetLogicalSize(renderer, mapWidth, mapHeight);
+    SDL_RenderSetLogicalSize(renderer, MAP_WIDTH, MAP_HEIGHT);
 
     bool shouldClose = 0;
     while(!shouldClose){
@@ -45,14 +44,14 @@ int main(int argc, char** argv){
         }
 
         // Check every cell on the board 
-        for(int i = 0; i < mapHeight; i++){            
-            for(int j = 0; j < mapWidth; j++){
+        for(int i = 0; i < MAP_HEIGHT; i++){            
+            for(int j = 0; j < MAP_WIDTH; j++){
                 unsigned char surroundingCells = 0;
                 // Check 8 surrounding cells
                 for(int ii = -1; ii <= 1; ii++){
                     for(int jj = -1; jj <= 1; jj++){
                         if(jj == 0 && ii == 0) continue;
-                        if(firstPointer[(ii + i) % mapHeight][(jj + j) % mapWidth]) surroundingCells++;
+                        if(firstPointer[(ii + i) % MAP_HEIGHT][(jj + j) % MAP_WIDTH]) surroundingCells++;
                     }
                 }
                 secondPointer[i][j] = firstPointer[i][j] ? (surroundingCells >= 2 && surroundingCells < 4) : (surroundingCells == 3);
@@ -60,7 +59,7 @@ int main(int argc, char** argv){
         }
 
         // Swap arrays (double-buffering)
-        unsigned char (*tempPointer)[mapHeight] = firstPointer;
+        unsigned char (*tempPointer)[MAP_HEIGHT] = firstPointer;
         firstPointer = secondPointer;
         secondPointer = tempPointer;
 
@@ -68,8 +67,8 @@ int main(int argc, char** argv){
 
         // Draw points
         SDL_SetRenderDrawColor(renderer, 255, 255, 255, 255);
-        for(int i = 0; i < mapHeight; i++){
-            for(int j = 0; j < mapWidth; j++){
+        for(int i = 0; i < MAP_HEIGHT; i++){
+            for(int j = 0; j < MAP_WIDTH; j++){
                 if(firstPointer[i][j]){
                     SDL_RenderDrawPoint(renderer, j, i);
                 }
